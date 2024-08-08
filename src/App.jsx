6b0a2107bff3b957d6f5cf73f2f852c2be703743
Marek Zelinka <mzelinka17@gmail.com>
2024-08-08 12:34:04 +0200

    declare winner

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+  const winner = calculateWinner(squares)
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? 'X' : 'O'}`
+
   const handleSquareClick = (i) => {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return
     }
 
@@ -17,6 +22,7 @@ export default function Board() {
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onClick={() => handleSquareClick(0)} />
         <Square value={squares[1]} onClick={() => handleSquareClick(1)} />
@@ -42,4 +48,25 @@ function Square({ value, onClick }) {
       {value}
     </button>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  for (const [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+
+  return null
 }
