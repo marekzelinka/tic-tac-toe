615618b30d30efbcd6a4e66ed03cebc680a654e3
Marek Zelinka <mzelinka17@gmail.com>
2024-08-08 08:20:13 +0200

    setup board

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
-export default function Square() {
-  return <button className="square">X</button>
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+    </>
+  )
 }
