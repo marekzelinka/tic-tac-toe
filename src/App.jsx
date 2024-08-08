2700d12c07b90d1d0f4f6e8d2f13756b6cb51610
Marek Zelinka <mzelinka17@gmail.com>
2024-08-08 12:31:25 +0200

    allow payers taking turns

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null))
 
   const handleSquareClick = (i) => {
+    if (squares[i]) {
+      return
+    }
+
     const nextSquares = squares.slice()
-    nextSquares[i] = 'X'
+    nextSquares[i] = xIsNext ? 'X' : '0'
     setSquares(nextSquares)
+    setXIsNext((xIsNext) => !xIsNext)
   }
 
   return (
