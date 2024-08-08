584fef99bedc676a9b7bf63ab0466c125c62cfbd
Marek Zelinka <mzelinka17@gmail.com>
2024-08-08 08:22:41 +0200

    pass props to components

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   )
+}
+
+function Square({ value }) {
+  return <button className="square">{value}</button>
 }
